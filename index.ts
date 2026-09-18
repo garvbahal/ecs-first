4956bcb331584e72992fc1782e1e0b9cf173aca2
garvbahal <garvbahal75@gmail.com>
2026-09-18 22:52:31 +0530

    added-express

--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
-console.log("Hello via Bun!");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("App is healthy");
+});
+
+app.get("/cpu", (req, res) => {
+  for (let i = 0; i <= 1_000_000_000; i++) {
+    Math.random();
+  }
+  res.send("Hello world");
+});
+
+app.listen(3000, () => {
+  console.log("App is running at 3000 port");
+});
